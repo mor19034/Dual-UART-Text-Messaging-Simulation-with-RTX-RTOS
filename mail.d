bfc18a31976384732d43d783a9078422f742ec89
PFMoreno <mor19034@uvg.edu.gt>
2026-06-11 14:19:32 -0400

    Working_Uart_With_Menu
    The communication channels now have a menu to choose to which UART the messages will be sent

--- mail.d
+++ mail.d
@@ -1,8 +1,8 @@
 ./mail.o: mail.c \
-  C:\Users\pablo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\STM32F10x.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\STM32F10x.h \
   RTE\_Target_1\RTE_Components.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS\RTX\INC\cmsis_os.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_LED.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS\RTX\INC\cmsis_os.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_LED.h \
   uart.h
--- system_stm32f10x.d
+++ system_stm32f10x.d
@@ -1,5 +1,5 @@
 ./system_stm32f10x.o: RTE\Device\STM32F103RB\system_stm32f10x.c \
-  C:\Users\pablo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\stm32f10x.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\stm32f10x.h \
   RTE\_Target_1\RTE_Components.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\pablo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\jmgoo\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h
